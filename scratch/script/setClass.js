32790d620138603b6e8f0213487c7ab520cba9ad
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2018-12-14 14:30:38 +0100

    console script is also moved to separeted file

--- scratch/script/setClass.js
+++ scratch/script/setClass.js
@@ -12,3 +12,8 @@ function clearClass(event) {
   let el = document.getElementById('mypara');
   el.className = '';
 }
+
+console.log('My code is runnig');
+function greet(name) {
+console.log('Hello, ' + name);
+}
