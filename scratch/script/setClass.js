c29aa6862701b973b6a871060ef383827616ad82
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2018-12-14 14:13:27 +0100

    script is separeted and added as separetely

---
+++ scratch/script/setClass.js
@@ -0,0 +1,14 @@
+function setClassTip(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'tip';
+}
+
+function setClassWarning(event) {
+  let el = document.getElementById('mypara');
+  el.className = 'warning';
+}
+
+function clearClass(event) {
+  let el = document.getElementById('mypara');
+  el.className = '';
+}
