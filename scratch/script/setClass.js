dcf57cec7de93d50624b5d59337dcbf96bb0c512
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2018-12-14 16:39:27 +0100

    function refactored

--- scratch/script/setClass.js
+++ scratch/script/setClass.js
@@ -1,19 +1,6 @@
-function setClassTip(event) {
+function setClass(event, className) {
   let el = document.getElementById('mypara');
-  el.className = 'tip';
+  el.className = className;
 }
 
-function setClassWarning(event) {
-  let el = document.getElementById('mypara');
-  el.className = 'warning';
-}
-
-function clearClass(event) {
-  let el = document.getElementById('mypara');
-  el.className = '';
-}
-
-console.log('My code is runnig');
-function greet(name) {
-console.log('Hello, ' + name);
-}
+console.log('ok')
