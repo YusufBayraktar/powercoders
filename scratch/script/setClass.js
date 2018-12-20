ccd169dbcf333a04a92649da80b4cc889c5e22e5
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2018-12-20 13:49:43 +0100

    changing className

--- scratch/script/setClass.js
+++ scratch/script/setClass.js
@@ -3,4 +3,19 @@ function setClass(event, className) {
   el.className = className;
 }
 
-console.log('ok')
+document.addEventListener('DOMContentLoaded', function(event) {
+  let makeWarning = document.getElementById('make-warning');
+  makeWarning.addEventListener('click', function(event) {
+  setClass(event, 'warning');
+  });
+
+  let makeTip = document.getElementById('make-tip');
+  makeTip.addEventListener('click', function(event) {
+    setClass(event, 'tip');
+  });
+
+  let makeNormal = document.getElementById('make-normal');
+  makeNormal.addEventListener('click', function(event) {
+    setClass(event, '');
+  });
+});
