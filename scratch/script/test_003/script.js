45558a6bd24a8430c76f8ce7f84f81c6796ce8d7
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2019-01-03 11:56:51 +0100

    onclick changeClass

--- scratch/script/test_003/script.js
+++ scratch/script/test_003/script.js
@@ -1,18 +1,22 @@
+alert('clickTHeCharacter');
+
 function hide(event) {
-  setClass('hide-icon', 'hide');
-  setClass('info', 'hide');
-  setClass('show-icon', 'show');
+  setClass('w', 'hide');
+  setClass('piece', 'hide');
+  setClass('light', 'show');
+  setClass('e', 'show');
+  setClass('body', 'violet'); 
 }
 
 function show(event) {
-  setClass('hide-icon', 'show');
-  setClass('info', 'show');
-  setClass('show-icon', 'hide');
+  setClass('w', 'show');
+  setClass('piece', 'show');
+  setClass('light', 'hide');
+  setClass('e', 'hide');
+  setClass('body', 'lightgrey'); 
+
 }
 
-
 function setClass(id, className) {
-    let el = document.getElementById(id);
-    el.className = className;
-
+  document.getElementById(id).className = className;
 }
