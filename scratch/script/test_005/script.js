f130587717dd61172d6ee70a213f8e254ff94c42
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2019-01-03 12:19:05 +0100

    keydown change style offset value +/- left

--- scratch/script/test_005/script.js
+++ scratch/script/test_005/script.js
@@ -1,3 +1,13 @@
-function move(event){
-    let el = document.getElementById
-}
+document.addEventListener('keydown', function(event){
+	let keyCode = event.keyCode;
+	let el = document.getElementById('figure');
+
+	if (keyCode == 39) {
+		el.style.left = el.offsetLeft + 18 + 'px';
+	}
+
+	if (keyCode == 37) {
+		el.style.left = el.offsetLeft - 18 + 'px';
+	}
+});
+
