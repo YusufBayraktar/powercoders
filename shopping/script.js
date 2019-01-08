9744d8b25d103b1e7b05a1e4df4049ef95a7314f
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2019-01-08 16:55:52 +0100

    indentated and extra spaces are deleted

--- shopping/script.js
+++ shopping/script.js
@@ -1,13 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(event){
   document.querySelector('button').addEventListener('click', function(event){
     let el = document.getElementById('item');
-
     let listItem = document.createElement('li');
     let listText = document.createElement('span');
     listText.textContent = el.value;
     let deleteButton = document.createElement('button');
     deleteButton.textContent = 'delete';
-
     deleteButton.addEventListener('click', function(){
       listItem.remove();
     });
@@ -15,8 +13,6 @@ document.addEventListener('DOMContentLoaded', function(event){
     listItem.appendChild(listText);
     listItem.appendChild(deleteButton);
     document.querySelector('ul').appendChild(listItem);
-
     return listItem;
-
   });
 });
