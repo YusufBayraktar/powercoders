b2510d5d9bcd0f2b34ee6cb34d8fd5b7ababba44
Yusuf Bayraktar <yusuf.bayraktar@powercoders.org>
2019-01-08 10:56:42 +0100

    script code of the add item button is written.

--- shopping/script.js
+++ shopping/script.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(event){
   document.querySelector('button').addEventListener('click', function(event){
-    let el = document.querySelector('#item').value
-    console.log(el);
+    let el = document.getElementById('item');
+
+    let listItem = document.createElement('li');
+    let listText = document.createElement('span');
+    listText.textContent = el.value;
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+
+    listItem.appendChild(listText);
+    listItem.appendChild(deleteButton);
+    document.querySelector('ul').appendChild(listItem);
+
+    return listItem;
+
   });
 });
