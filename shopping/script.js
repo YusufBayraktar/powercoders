d2b2c389bb316f9566842daa9e56512c462bc028
YusufBayraktar <yusuf.bayraktar@powercoders.org>
2019-01-29 16:07:18 +0100

    quantity input box is added

--- shopping/script.js
+++ shopping/script.js
@@ -5,7 +5,86 @@
  *
  * @param {string} itemName Name of the item to add to the listText
  * @returns {HTMLElement} li element
+ *//**
+ * Creates and returns an 'li' element for inclusion in the shopping list.
+ *
+ * should be a blank line between paragraphs
+ *
+ * @param {string} itemName Name of the item to add to the listText
+ * @param {string} quantity quantity of the added item
+ * @returns {HTMLElement} li element
  */
+function createNewListItem(itemName, quantity) {
+  const listItem = document.createElement('li');
+  const listText = document.createElement('span');
+  const quantityOfProduct = document.createElement('span');
+  listText.textContent = itemName;
+  space = document.createTextNode(' ');
+  quantityOfProduct.textContent = quantity;
+  const deleteButton = document.createElement('button');
+  deleteButton.className = "fas fa-trash";
+  deleteButton.addEventListener('click', function (event) {
+    listItem.remove();
+    document.querySelector('input').focus();
+  });
+
+  listItem.appendChild(quantityOfProduct);
+  listItem.appendChild(space);
+  listItem.appendChild(listText);
+  listItem.appendChild(deleteButton);
+  return listItem;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  const inputBox = document.getElementById('item');
+  const inputBoxQuantity = document.getElementById('theQuantity');
+  const shoppingList = document.querySelector('ul');
+  const button = document.querySelector('button');
+  inputBox.focus();
+  button.disabled = true;
+
+  button.addEventListener('click', function (event) {
+    const trimmedValue = inputBox.value.trim();
+    button.disabled = trimmedValue === '';
+    shoppingList.appendChild(createNewListItem(trimmedValue, inputBoxQuantity.value.trim()));
+    inputBox.value = '';
+    inputBoxQuantity.value = '';
+    button.disabled = true;
+    inputBox.focus();
+  });
+
+  inputBox.addEventListener('keyup', function (event) {
+    const trimmedValue = inputBox.value.trim();
+    button.disabled = trimmedValue === '';
+    if (trimmedValue === '') {
+      return;
+    }
+
+   if (event.key !== 'Enter') {
+      return;
+    }
+    shoppingList.appendChild(createNewListItem(trimmedValue, inputBoxQuantity.value.trim()));
+    inputBox.value = '';
+    inputBoxQuantity.value = '';
+    button.disabled = true;
+    inputBox.focus();
+    
+   if (inputBoxQuantity.value.trim() !== '') {
+     shoppingList.appendChild(createNewListItem(trimmedValue, inputBoxQuantity.value.trim()));
+     inputBox.value = '';
+     inputBoxQuantity.value = '';
+     button.disabled = true;
+     inputBox.focus();  
+    }
+  });
+
+  const delAll = document.getElementById('del');
+  delAll.addEventListener('click', function(event) {
+    const lists = document.querySelectorAll('li');
+    for (i = 0; i < lists.length; i++)
+          lists[i].remove();
+  });
+});
 function createNewListItem(itemName) {
   const listItem = document.createElement('li');
   const listText = document.createElement('span');
