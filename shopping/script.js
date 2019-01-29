dfa7cb52074a0b1dbf39968ce8168829267498ec
YusufBayraktar <yusuf.bayraktar@powercoders.org>
2019-01-29 16:31:50 +0100

    false code was added before, now it works. quantity box does not yet have keyup eventlistener

--- shopping/script.js
+++ shopping/script.js
@@ -4,13 +4,6 @@
  * should be a blank line between paragraphs
  *
  * @param {string} itemName Name of the item to add to the listText
- * @returns {HTMLElement} li element
- *//**
- * Creates and returns an 'li' element for inclusion in the shopping list.
- *
- * should be a blank line between paragraphs
- *
- * @param {string} itemName Name of the item to add to the listText
  * @param {string} quantity quantity of the added item
  * @returns {HTMLElement} li element
  */
@@ -76,60 +69,6 @@ document.addEventListener('DOMContentLoaded', function (event) {
      button.disabled = true;
      inputBox.focus();  
     }
-  });
-
-  const delAll = document.getElementById('del');
-  delAll.addEventListener('click', function(event) {
-    const lists = document.querySelectorAll('li');
-    for (i = 0; i < lists.length; i++)
-          lists[i].remove();
-  });
-});
-function createNewListItem(itemName) {
-  const listItem = document.createElement('li');
-  const listText = document.createElement('span');
-  listText.textContent = itemName;
-  const deleteButton = document.createElement('button');
-  deleteButton.className = "fas fa-trash";
-  deleteButton.addEventListener('click', function (event) {
-    listItem.remove();
-    document.querySelector('input').focus();
-  });
-
-  listItem.appendChild(listText);
-  listItem.appendChild(deleteButton);
-  return listItem;
-}
-
-document.addEventListener('DOMContentLoaded', function (event) {
-  const inputBox = document.getElementById('item');
-  const shoppingList = document.querySelector('ul');
-  const button = document.querySelector('button');
-  inputBox.focus();
-  button.disabled = true;
-
-  button.addEventListener('click', function (event) {
-    const trimmedValue = inputBox.value.trim();
-    button.disabled = trimmedValue === '';
-    shoppingList.appendChild(createNewListItem(trimmedValue));
-    inputBox.value = '';
-    button.disabled = true;
-    inputBox.focus();
-  });
-
-  inputBox.addEventListener('keyup', function (event) {
-    const trimmedValue = inputBox.value.trim();
-    button.disabled = trimmedValue === '';
-    if (trimmedValue === '') {
-      return;
-    }
-    if (event.key !== 'Enter') {
-      return;
-    }
-    shoppingList.appendChild(createNewListItem(trimmedValue));
-    inputBox.value = '';
-    button.disabled = true;
-    inputBox.focus();
   });
 
   const delAll = document.getElementById('del');
