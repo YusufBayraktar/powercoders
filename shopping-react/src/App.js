306983add2ac0b7a5a5b76785d7536aa54371c6b
YusufBayraktar <yusuf.bayraktar@powercoders.org>
2019-02-25 15:09:31 +0100

    react displays ul>il

--- shopping-react/src/App.js
+++ shopping-react/src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ItemInput from './ItemInput';
+import ClearList from './ClearList';
+import ShoppingList from './ShoppingList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ['fn', 'ln', 'int'],
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <ItemInput/>
+        <ClearList/>
+        <ShoppingList items={this.state.items}/>
       </div>
+
+      //<div className="App">
+        //<div className="App-header">
+          //<img src={logo} className="App-logo" alt="logo" />
+          //<h2>Welcome to React</h2>
+        //</div>
+        //<p className="App-intro">
+          //To get started, edit <code>src/App.js</code> and save to reload.
+        //</p>
+      //</div>
     );
   }
 }
