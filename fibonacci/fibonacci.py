6611a597352e3576109f43eeb2f080b4d03186cc
YusufBayraktar <yusuf.bayraktar@powercoders.org>
2019-03-05 16:39:58 +0100

    calculates also golden ratio around .nth element

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,4 +1,5 @@
 x = int(input('.nth of fibonacci: '))
+z = int(input('golden ratio of .nth fibonacci: '))
 
 
 def fib(x):
@@ -16,5 +17,23 @@ def fib(x):
     return arr[x]
 
 
+def gr(z):
+    arr = [1, 1]
+
+    for t in arr:
+        if t < 1:
+            pass
+        elif 55 < t:
+            pass
+        else:
+            t = arr[-1] + arr[-2]
+            arr.append(t)
+
+    golden_ratio = arr[-2] / arr[-1]
+
+    return golden_ratio
+
+
 print(fib(x))
+print(gr(z))
 
